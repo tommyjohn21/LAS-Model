063dd85390f5a34c67bf55137249ee02f1eec1e9
Tommy Wilson <tommyjohn21@gmail.com>
2021-07-30 12:24:40 -0400

    Adjusted to shock the same network 20 times by 5 simulations

---
+++ dW/simulate_dW_converge.m
@@ -0,0 +1,21 @@
+%% Use Exp5STDP.m script to generate 100 simulations of weight matrix
+
+savedir = '~/dW_converge';
+vardir = '~/dW';
+sim = 5;
+nseizures = 20;
+
+parfor s = 1:sim
+    for i = 2:nseizures
+        
+        % Annouce entry
+        disp(['Beginning seizure ' num2str(i) ' for dW ' num2str(s) '...'])
+        
+        % Generate and retrieve weight matrices
+        o = Exp5STDP_converge(i,savedir,s);
+                
+        % Save output in parallel fashion
+        parsave([savedir '/dW_' num2str(s) '_converge_sim_' num2str(i) '.mat'],o)
+        
+    end
+end
