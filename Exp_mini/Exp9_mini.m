70a1c4d805b53741dc3832505b29b141f6c3db12
Tommy Wilson <tommyjohn21@gmail.com>
2021-10-11 16:48:42 -0500

    Added code to parse STDP matrices into two types

--- Exp_mini/Exp9_mini.m
+++ Exp_mini/Exp9_mini.m
@@ -5,20 +5,59 @@
 sbm = simulation_bin_mini;
 
 %% Create average dW matrix
-f = dir('~/Desktop/Exp6_mini/');
+f = dir('~/Exp6_mini/');
 D = [];
+F = [];
 for i = 1:numel(f)
     if strcmp(f(i).name,'.') || strcmp(f(i).name,'..') || any(strfind(f(i).name,'.DS_Store')) || any(strfind(f(i).name,'.txt')) || any(strfind(f(i).name,'Wn')), continue, end
     load([f(i).folder '/' f(i).name])
-    D = cat(3,D,d.dW);
+    F = cat(3,F,lowpass(lowpass(d.dW,10/500,'ImpulseResponse','iir').',10/500,'ImpulseResponse','iir').');  
+    D = cat(3,D,d.dW);  
 end
-% Change of variable for consistency with code below
-dWave = mean(D,3);
 
-%% Upweight dWave
-scalar = 1.1; % produces max value in central stim that is the same as max value in edge stim
+%% Compute nodes/types
 
-dWave = scalar.*dWave;
+% Pull out fixed points
+C = zeros(100,size(F,2)-1);
+for j = 1:100
+    ch = F(:,:,j).*Wn-Wn;
+    w = nan(51,numel(diag(ch)));
+    for i = 0:50
+        w(i+1,i+2:end)=diag(ch,-(i+1));
+    end
+    C(j,:) = fixed_points(nansum(w));
+end
+C(:,1:50) = 0; % Don't count edges (numerically unstable, presumably)
+C(:,end-49:end) = 0; % Don't count edges 
+type = sum(C>1,2); % Type 1: single node, Type 2: stable node exists
+
+% Decide which type 1 to flip
+flip = zeros(100,1);
+for i = 1:100
+    if type(i)==1 && find(C(i,:))>250
+       flip(i) = -1;
+    elseif type(i) == 2
+        flip(i) = 0;
+    else
+       flip(i) = 1; 
+    end
+    
+end
+
+R = D; RF = F;
+for i = 1:size(F,3)
+    if flip(i)==1 || flip(i)==0
+        continue
+    else
+       R(:,:,i) = rot90(R(:,:,i),2);
+       RF(:,:,i) = rot90(RF(:,:,i),2);
+    end
+    
+end
+
+% Parse out two types of STDP matrix
+dW1 = mean(R(:,:,type==1),3);
+dW2 = mean(R(:,:,type==2),3);
 
 %% Experiment parameters
 % Call default params
@@ -28,12 +67,20 @@ param = sbm.simulate_mini_model('get_defaults');
 param.stim_location = [0.475 0.525];
 param.threshold_reptitions = 100;
 param.threshold_stimulations = [1:0.05:3];
-param.threshold_savedir = '~/Exp7_mini/';
+param.threshold_savedir = '~/Exp9_mini/Type1';
 % param.flag_return_voltage_trace = true;
 % param.flag_return_state_trace = true;
 % param.flag_kill_if_seizure = true;
 % param.flag_kill_if_wave_collapsed = true;
-param.dW_matrix = dWave;
+param.dW_matrix = dW1;
 
-%% Run experiment with parameters
+%% Run type 1 experiment with parameters
 sbm.find_mini_model_threshold(param)
+
+%% Run type 2 experiment with parameters
+param.dW_matrix = dW2;
+param.threshold_savedir = '~/Exp9_mini/Type2';
+
+sbm.find_mini_model_threshold(param)
+
+
