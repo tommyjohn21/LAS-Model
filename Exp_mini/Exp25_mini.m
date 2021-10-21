7c519f3bf3aac3073296bb0dd9d371280a7c1275
Tommy Wilson <tommyjohn21@gmail.com>
2021-10-21 13:06:41 -0500

    Added Exp25 for determination of noise threshold in stabilized model after 1 round with dWave

--- Exp_mini/Exp25_mini.m
+++ Exp_mini/Exp25_mini.m
@@ -1,4 +1,5 @@
-%%% Detemine mini model seizure threshold for the naive stabilized eLife model (in Exp5)
+%%% Detemine mini model seizure threshold for the stabilized eLife model
+%%% after application of dWave (in Exp5)
 
 %% Access simulation_bin_mini tools
 sbm = simulation_bin_mini;
@@ -11,7 +12,7 @@ param = sbm.simulate_mini_model('get_defaults');
 % param.stim_location = [0.475 0.525];
 param.threshold_reptitions = 100;
 param.threshold_stimulations = 0;
-param.threshold_sigmas = [20:5:60];
+param.threshold_sigmas = 5:5:40; %[20:5:60];
 param.flag_deterministic = 0;
 % param.flag_return_voltage_trace = true;
 % param.flag_return_state_trace = true;
@@ -27,19 +28,19 @@ param.dilate = 1;
 
 %% Create and set average dW matrix
 % Load average dW matrix
-% f = dir('~/Desktop/Exp22_mini/');
-% D = [];
-% F = [];
-% for i = 1:numel(f)
-%     if any(strfind(f(i).name,'Wn')), load([f(i).folder '/' f(i).name]), continue, end
-%     if ~any(strfind(f(i).name,'.mat')), continue, end
-%     load([f(i).folder '/' f(i).name])
-%     fprintf(['Loading ' f(i).folder '/' f(i).name '...\n'])
-%     F = cat(3,F,lowpass(lowpass(d.dW,10/500,'ImpulseResponse','iir').',10/500,'ImpulseResponse','iir').');  
-%     D = cat(3,D,d.dW);  
-% end
-% 
-% param.dW_matrix = mean(D,3);
+f = dir('~/Desktop/Exp22_mini/');
+D = [];
+F = [];
+for i = 1:numel(f)
+    if any(strfind(f(i).name,'Wn')), load([f(i).folder '/' f(i).name]), continue, end
+    if ~any(strfind(f(i).name,'.mat')), continue, end
+    load([f(i).folder '/' f(i).name])
+    fprintf(['Loading ' f(i).folder '/' f(i).name '...\n'])
+    F = cat(3,F,lowpass(lowpass(d.dW,10/500,'ImpulseResponse','iir').',10/500,'ImpulseResponse','iir').');
+    D = cat(3,D,d.dW);
+end
+
+param.dW_matrix = mean(D,3);
 
 %% Run experiment with parameters
 sbm.find_mini_model_threshold(param)
