f76c730505d961696b9bfd09d04367730132fe6b
Tommy Wilson <tommyjohn21@gmail.com>
2024-02-27 15:34:24 -0500

    Rewrote ThresholdExperiment/Run script to run Simulations in parallel instead of running levels of background noise in parallel

--- @ThresholdExperiment/Run.m
+++ @ThresholdExperiment/Run.m
@@ -22,53 +22,69 @@ if E.S.O.t>0, Reset(E.S); end
 % Create ExpDir if doesn't exist
 if ~exist(E.param.expdir,'dir'), mkdir(E.param.expdir); end
 
-%%% Conditional parallel computation %%%
-% Grand data concatenation for passage in parfor
-levels = E.param.inputs.levels;
-TE = cellfun(@(x)copy(E),num2cell(1:numel(levels)),'un',0);
-if E.param.server || E.param.flags.parallel
-    parfor (i = 1:numel(levels)), ExecuteSimulations(TE{i},i); end
-else
-    for i = 1:numel(levels), ExecuteSimulations(TE{i},i); end
-end
+% Execute Simulations for each level
+for i = 1:numel(E.param.inputs.levels), ExecuteSimulations(E,i); end
+
 end
 
-function ExecuteSimulations(TE,i)
+function ExecuteSimulations(E,i)
 
         % Pull local Simulation handle
-        S = TE.S;
+        S = E.S;
+
+        % Copy Simulation object to copy after each simulation run
+        T = copy(S);
         
-        % Identify local level/type for ThresholdExperiment for readability
-        param = TE.param;
+        % Identify local run parameters for readability
+        param = E.param;
         type = param.inputs.type;
         level = param.inputs.levels(i);
+        n = param.n;
         
         % Update input
         UpdateInput(S,type,level);
-        
-        % Initialize output structures
-        detector = []; % Empty detector for concatenation
-        seed = []; % Empty seed container for concatenation
         
         % Skip Simulation if already performed
         FileName = sprintf([param.expdir 'ThresholdExperiment-Level-%0.3f.mat'],level);
         if exist(FileName,'file'), return, end
         
+        % Initialize parallelizable output structures
+        detector = cell(1,n); % Empty detector for concatenation
+        seed = cell(1,n); % Empty seed container for concatenation
+
         % Run simulations
-        for j = 1:param.n
-            fprintf('Level %0.3f, simulation %i of %i\n',level,j,param.n)
-            Run(S)
-            detector = [detector S.detector]; % Append detector from each simulation
-            seed = [seed S.seed]; % Retain seeds used in each simulation
-            Reset(S)
+        if E.param.server || E.param.flags.parallel
+            parfor (j = 1:n), [d,s] = Simulate(S,j,level,n); detector{j} = d; seed{j} = s; end
+        else
+            for j = 1:n, [d,s] = Simulate(S,j,level,n); detector{j} = d; seed{j} = s; end
         end
         
         % Append completed results
-        TE.S.detector = detector;
-        TE.S.seed = seed;
+        E.S.detector = [detector{:}];
+        E.S.seed = [seed{:}];
 
         % Save output
-        parsave(FileName,TE)
+        parsave(FileName,E)
+
+        % Reset Simulation object, detector and seed in particular,
+        E.S = T;
+
+end
+
+function [detector, seed] = Simulate(S,j,level,n)
+    
+    % Update user
+    fprintf('Level %0.3f, simulation %i of %i\n',level,j,n)
+
+    % Run the simulation
+    Run(S); 
+
+    % Reset for next loop
+    Reset(S);
+    
+    % Collect output variables for pass up stack
+    detector = S.detector; % Append detector from each simulation
+    seed = S.seed; % Retain seeds used in each simulation 
 
 end
 
